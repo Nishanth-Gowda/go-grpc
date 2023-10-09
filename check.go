4ffb981e2d047840150824afd441b3da1232e3f3
EC2 Default User <ec2-user@ip-172-31-3-98.ap-south-1.compute.internal>
2023-10-09 10:59:19 +0000

    Added Response Time Feature

--- check.go
+++ check.go
@@ -9,13 +9,20 @@ import (
 func Check(destination string, port string) string {
 	address := destination + ":" + port
 	timeout := 5 * time.Second
+	startTime := time.Now() // Record the start time
+
 	conn, err := net.DialTimeout("tcp", address, timeout)
+
+	// Calculate the response time
+	responseTime := time.Since(startTime)
+
 	var status string
 
 	if err != nil {
 		status = fmt.Sprintf("[DOWN!] %v is unreachable, \n Error: %v", destination, err)
 	} else {
-		status = fmt.Sprintf("[UP :) & RUNNING] %v is reachable,\n From: %v\n To: %v", destination, conn.LocalAddr(), conn.RemoteAddr())
+		status = fmt.Sprintf("[UP :) & RUNNING] %v is reachable,\n From: %v\n To: %v\n Response Time: %v", destination, conn.LocalAddr(), conn.RemoteAddr(), responseTime)
 	}
+
 	return status
 }
