8e48bccac722286f40d52a3a67ac1693d0c70675
EC2 Default User <ec2-user@ip-172-31-3-98.ap-south-1.compute.internal>
2023-10-09 10:30:50 +0000

    Initial Commit

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "Health-Checker",
+		Usage: "A tool that checks health of website, i.e, Up & Running or is down",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "domain",
+				Aliases:  []string{"d"},
+				Usage:    "Domain name to check",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "port",
+				Aliases:  []string{"p"},
+				Usage:    "Port number to check",
+				Required: false,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			port := cCtx.String("port")
+			if cCtx.String("port") == "" {
+				port = "80"
+			}
+			status := Check(cCtx.String("domain"), port)
+			fmt.Println(status)
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
